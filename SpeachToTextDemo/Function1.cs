1be09057c3dd654e5b763094855351de7d23e801
Tamami Ihori <tamamiihori@microsoft.com>
2025-09-22 16:42:53 +0900

    add function project

---
+++ SpeachToTextDemo/Function1.cs
@@ -0,0 +1,24 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace SpeachToTextDemo;
+
+public class Function1
+{
+    private readonly ILogger<Function1> _logger;
+
+    public Function1(ILogger<Function1> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(Function1))]
+    public async Task Run([BlobTrigger("audio-in/{name}/{name}", Source = BlobTriggerSource.EventGrid, Connection = "")] Stream stream, string name)
+    {
+        using var blobStreamReader = new StreamReader(stream);
+        var content = await blobStreamReader.ReadToEndAsync();
+        _logger.LogInformation("C# Blob Trigger (using Event Grid) processed blob\n Name: {name} \n Data: {content}", name, content);
+    }
+}
